32383f2016a9ebaa0aac2aa2bdb06e1a193a37f1
kang-heewon <itphantom@icloud.com>
2020-02-04 07:31:29 +0900

    feat:⚡️patients serializing & view

--- patients/models.py
+++ patients/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class Patient(models.Model):
+    index = models.IntegerField()
+    date = models.DateField()
+    status = models.CharField(max_length=30)
+    contact_count = models.IntegerField()
+    second_infection = models.IntegerField()
+
+    def __str__(self):
+        return "%s %s %s %s" % (self.index, self.date, self.status, self.contact_count)
+
+
+class Movement(models.Model):
+    date = models.DateField()
+    place = models.CharField(max_length=20)
+    address = models.CharField(max_length=100)
+    patient = models.ForeignKey(
+        Patient, on_delete=models.CASCADE, on_update=models.CASCADE)
+
+    def __str__(self):
+        return self.address
---
+++ patients/serializers.py
@@ -0,0 +1,8 @@
+# 모델 설정 fields = ('id','title','genre','year') # 필드 설정
+from rest_framework import serializers
+from .models import Patient
+
+
+class PatientSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Patient
