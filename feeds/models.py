ce9143a4375cd7ea7b1692c3344a32539c1a95ab
kang-heewon <itphantom@icloud.com>
2020-02-07 01:46:52 +0900

    fix:🔥 feed error

--- feeds/models.py
+++ feeds/models.py
@@ -5,7 +5,7 @@ class Feed(models.Model):
     date = models.DateField()
     place = models.CharField(max_length=20, blank=True)
     status = models.CharField(max_length=200, blank=True)
-    contact_count = models.IntegerField(blank=True)
+    contact_count = models.IntegerField(blank=True, null=True)
     second_infection = models.IntegerField(blank=True, null=True)
     lat = models.FloatField(blank=True)
     lng = models.FloatField(blank=True)
