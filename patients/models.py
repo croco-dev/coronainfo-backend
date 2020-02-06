e0356f7e4b171356a9409972ebba678ec1eeb604
kang-heewon <itphantom@icloud.com>
2020-02-07 01:43:49 +0900

    fix:🔥set default value

--- patients/models.py
+++ patients/models.py
@@ -5,7 +5,7 @@ class Patient(models.Model):
     index = models.IntegerField()
     date = models.DateField()
     status = models.CharField(max_length=200)
-    contact_count = models.IntegerField()
+    contact_count = models.IntegerField(default=0)
     lat = models.FloatField(blank=True)
     lng = models.FloatField(blank=True)
     second_infection = models.IntegerField(blank=True, null=True)
