c8ee710f206d8487546f4c3ec8dbc21e78ead532
kang-heewon <itphantom@icloud.com>
2020-02-09 01:55:27 +0900

    fix:🔥import modeling

--- versions/views.py
+++ versions/views.py
@@ -1,11 +1,12 @@
 from rest_framework import viewsets, filters
 from feeds.serializers import FeedsSerializer
 from coronainfo.permissions import IsAdminOrReadOnly
+from .models import Version
 from .serializers import VersionSerializer
 
 
 class VersionViewSet(viewsets.ModelViewSet):
-    queryset = Movement.objects.all()
+    queryset = Version.objects.all()
     serializer_class = VersionSerializer
     filter_backends = [filters.OrderingFilter]
     ordering = "-date"
