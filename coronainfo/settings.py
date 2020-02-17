bb6e682f37810964083b09ce052075bdbc3f7407
kang-heewon <itphantom@icloud.com>
2020-02-17 09:18:01 +0900

    fix:🔥 disallow other sites

--- coronainfo/settings.py
+++ coronainfo/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = "qr$q_#3&_d3s%xqu#2%n0qlmx(cji$zn7$w15^4i%hkgo!#y3^"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["coronas.info", "127.0.0.1"]
 
 
 # Application definition
