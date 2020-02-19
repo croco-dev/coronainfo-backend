8d55475b4552a09c1a7fac9a195a167b08056a2f
kang-heewon <itphantom@icloud.com>
2020-02-20 01:27:54 +0900

    fix:🔥sex set char

--- patients/models.py
+++ patients/models.py
@@ -11,7 +11,7 @@ class Patient(models.Model):
     infected_route = models.CharField(max_length=300, null=True)
     hospital = models.CharField(max_length=30, null=True)
     age = models.IntegerField(null=True)
-    sex = models.IntegerField(null=True)
+    sex = models.CharField(max_length=10, null=True)
     country = models.CharField(max_length=30, null=True)
 
     def __str__(self):
