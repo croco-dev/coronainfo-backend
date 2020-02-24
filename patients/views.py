b811e06dea949aee5178f74e60bd83b5aef638e5
kang-heewon <itphantom@icloud.com>
2020-02-24 22:36:11 +0900

    fix:🔥patient retrieve

--- patients/views.py
+++ patients/views.py
@@ -2,6 +2,8 @@ from rest_framework import viewsets, filters
 from feeds.serializers import FeedsSerializer
 from .serializers import PatientSerializer
 from .models import Patient
+from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
 
 
 class PatientViewSet(viewsets.ModelViewSet):
@@ -9,6 +11,11 @@ class PatientViewSet(viewsets.ModelViewSet):
     serializer_class = PatientSerializer
     filter_backends = [filters.OrderingFilter]
     ordering = "-index"
+    def retrieve(self, request, pk=None):
+        queryset = Patient.objects.all()
+        patient = get_object_or_404(queryset, index=pk)
+        serializer = PatientSerializer(patient)
+        return Response(serializer.data)
 
     def perform_create(self, serializer):
         data = self.request.data
