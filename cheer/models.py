077141666fa1676694b256cf2e1ceef60878dad2
kang-heewon <itphantom@icloud.com>
2020-03-03 16:03:18 +0900

    fix:🔥add tags

--- cheer/models.py
+++ cheer/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
 
 # Create your models here.
 
 class Cheer(models.Model):
   ip = models.GenericIPAddressField()
   content = models.TextField()
+  tags = ArrayField(models.CharField(max_length=200),blank=True)
   created_at = models.DateTimeField(auto_now_add=True)
